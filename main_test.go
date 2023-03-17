a1850ba54e4b59553727c2baa103619b3d916fb0
Matt Finkel <finkel.matt@gmail.com>
2023-03-17 13:06:40 +0200

    Add test skeleton

---
+++ main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	_ "embed"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+//go:embed testdata/001_valid.csv
+var valid string
+
+//go:embed testdata/002_invalid.csv
+var invalid string
+
+func TestHelloWorld(t *testing.T) {
+	assert.NotEmpty(t, valid)
+	assert.NotEmpty(t, invalid)
+}
