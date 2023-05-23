3a742dfc7fdecd945a67aca83c0acba6da5bb1e9
Matt Finkel <finkel.matt@gmail.com>
2023-05-23 23:26:43 +0200

    Add logging to server handler method

--- cmd/sensus-mwater-adapter/server.go
+++ cmd/sensus-mwater-adapter/server.go
@@ -21,20 +21,24 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	// check authentication
 	username, password, ok := r.BasicAuth()
 	if !ok {
+		log.Println("invalid basic auth")
 		http.Error(w, "unauthorized", http.StatusUnauthorized)
 		return
 	}
 	if username != adapterUsername {
+		log.Printf("unauthorized username: %s", username)
 		http.Error(w, "unauthorized", http.StatusUnauthorized)
 		return
 	}
 	if password != adapterPassword {
+		log.Println("unauthorized password")
 		http.Error(w, "unauthorized", http.StatusUnauthorized)
 		return
 	}
 
 	// check method
 	if r.Method != "POST" {
+		log.Printf("disallowed method: %s", r.Method)
 		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
@@ -42,6 +46,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	// check file size
 	r.Body = http.MaxBytesReader(w, r.Body, maxUploadSize)
 	if err := r.ParseMultipartForm(maxUploadSize); err != nil {
+		log.Printf("multipart form error: %s", err.Error())
 		http.Error(w, fmt.Sprintf("multipart form error: %s", err.Error()), http.StatusBadRequest)
 		return
 	}
@@ -49,6 +54,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	// check properly formed request
 	file, fileHeader, err := r.FormFile("file")
 	if err != nil {
+		log.Printf("file format error: %s", err.Error())
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
@@ -58,22 +64,28 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	buff := make([]byte, 512)
 	_, err = file.Read(buff)
 	if err != nil {
+		log.Printf("error reading buffer: %s", err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	filetype := http.DetectContentType(buff)
+	// TODO why octet-stream?
+	// if !strings.HasPrefix(filetype, "text/") || !strings.HasPrefix(filetype, "application/octet-stream") {
 	if !strings.HasPrefix(filetype, "text/") {
+		log.Printf("file format not allowed: %s", filetype)
 		http.Error(w, fmt.Sprintf("file format not allowed: %s", filetype), http.StatusBadRequest)
 		return
 	}
 	_, err = file.Seek(0, io.SeekStart)
 	if err != nil {
+		log.Printf("error reading uploaded file: %s\n", err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	err = os.MkdirAll("./uploads", os.ModePerm)
 	if err != nil {
+		log.Printf("error making local file dir: %s\n", err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -87,6 +99,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 
 	dst, err := os.Create(fmt.Sprintf("./uploads/%s%s", f, filepath.Ext(fileHeader.Filename)))
 	if err != nil {
+		log.Printf("error creating local file [%s]: %s\n", f, err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -95,6 +108,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 
 	_, err = io.Copy(dst, file)
 	if err != nil {
+		log.Printf("error writing local file [%s]: %s\n", dst.Name(), err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -103,7 +117,9 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	data, err := os.Open(filename)
 	if err != nil {
 		log.Printf("error opening csv [%s]: %s\n", filename, err.Error())
-		os.Exit(1)
+		// os.Exit(1)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	sensusReadings, errs := sensus.ParseCSV(data)
@@ -111,19 +127,22 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		for _, err := range errs {
 			log.Printf("error parsing csv: %s\n", err.Error())
 		}
-		os.Exit(1)
+		http.Error(w, "errors parsing some csv lines", http.StatusInternalServerError)
+		return
 	}
 
 	mWaterClient, err := mwater.NewClient(mWaterBaseURL, mWaterUsername, mWaterPassword, dryRun)
 	if err != nil {
 		log.Printf("error setting up mwater client: %s\n", err.Error())
-		os.Exit(1)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	err = sync(mWaterClient, sensusReadings)
 	if err != nil {
 		log.Printf("error syncing sensus readings to mwater transaction: %s\n", err.Error())
-		os.Exit(1)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	fmt.Fprintf(w, "submission received")
