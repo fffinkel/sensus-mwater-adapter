d52aad61c77532adceb3fcfc132004e635c51ff3
Matt Finkel <finkel.matt@gmail.com>
2023-06-11 00:27:02 +0200

    Add server run script

---
+++ testserver.sh
@@ -0,0 +1,9 @@
+bin/sensus-mwater-adapter \
+  --mwater-to-account "todo" \
+  --mwater-from-account "todo" \
+  --port 9090 \
+  --dry-run \
+  server
+  # --mwater-base-url "https://api.mwater.co/v3/" \
+  # --mwater-username "exampleuser" \
+  # --mwater-password "A#QB2B8I)bN\$AVYApxzG" \
