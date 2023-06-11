13fd03d7132183cdcdf6e216ac207803bd05eb86
Matt Finkel <finkel.matt@gmail.com>
2023-06-11 11:46:49 +0200

    Add a script to make testing a big quicker
    Bypass the client server stuff and process CSVs directly.

---
+++ testapp.sh
@@ -0,0 +1,8 @@
+bin/sensus-mwater-adapter \
+  --mwater-to-account "" \
+  --mwater-from-account "" \
+  --mwater-base-url "https://api.mwater.co/v3" \
+  --mwater-username "" \
+  --mwater-password "" \
+  $@
+  # --dry-run \
